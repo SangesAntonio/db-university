25654bd68aa56a1797969372eec4418edc5e7ccd
SangesAntonio <sanges65@gmail.com>
2022-03-09 15:42:03 +0100

    added 6 - 7 and 8

--- esercizio.sql
+++ esercizio.sql
@@ -2,19 +2,14 @@
 SELECT * 
 FROM `students` 
 WHERE YEAR (`date_of_birth`)= 1990 
-
-
 --2. Selezionare tutti i corsi che valgono più di 10 crediti (479)
 SELECT * 
 FROM `courses` 
 WHERE (`cfu`) >= 10 
-
-
 --3. Selezionare tutti gli studenti che hanno più di 30 anni
 SELECT * 
 FROM `students` 
 WHERE YEAR (`date_of_birth`) >= 1992 
-
 --4. Selezionare tutti i corsi del primo semestre del primo anno di un qualsiasi corso dilaurea (286)
 SELECT * 
 FROM `courses` 
@@ -26,15 +21,15 @@ SELECT *
 FROM `exams` 
 WHERE HOUR(`hour`) >= '14:00'
 AND (`date`) = '2020-06-20'
-
-
-
 --6. Selezionare tutti i corsi di laurea magistrale (38)
-
+SELECT * 
+FROM `degrees` 
+WHERE (`name`) LIKE 'Corso di laurea Magistrale%'
 
 
 --7. Da quanti dipartimenti è composta l'università? (12)
-
+SELECT COUNT(*) AS 'numero di dipartimenti' FROM `departments` 
 
 
 --8. Quanti sono gli insegnanti che non hanno un numero di telefono? (50)--
+SELECT * FROM `teachers` WHERE (`phone`) IS NULL 
