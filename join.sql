057f7907fc0493bd4d6a30f7645c3a4812cf78c3
SangesAntonio <sanges65@gmail.com>
2022-03-10 15:14:22 +0100

    added n.3

--- join.sql
+++ join.sql
@@ -12,9 +12,15 @@ JOIN `departments`
 ON `departments`.`id`= `degrees`.`department_id`
 WHERE `departments`.`name` = 'Dipartimento di Neuroscienze'
 
-
-
 --3. Selezionare tutti i corsi in cui insegna Fulvio Amato (id=44)
+SELECT *
+FROM `courses`
+JOIN `course_teacher`
+ON `courses`.`id`= `course_teacher`. `course_id`
+JOIN `teachers`
+ON `course_teacher`. `teacher_id`= `teachers`.`id`
+WHERE `teachers`.`name` = 'fulvio' AND `teachers`.`surname` = 'amato'
+
 --4. Selezionare tutti gli studenti con i dati relativi al corso di laurea a cui sono iscritti e il relativo dipartimento, in ordine alfabetico per cognome e nome
 --5. Selezionare tutti i corsi di laurea con i relativi corsi e insegnanti
 --6. Selezionare tutti i docenti che insegnano nel Dipartimento di Matematica (54)
