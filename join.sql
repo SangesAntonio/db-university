123658012100a752ef83f4dd83f87645a2ddffaa
SangesAntonio <sanges65@gmail.com>
2022-03-10 17:29:43 +0100

    fixed 6

--- join.sql
+++ join.sql
@@ -41,7 +41,7 @@ JOIN `teachers`
 ON `course_teacher`. `teacher_id`= `teachers`.`id`
 
 --6. Selezionare tutti i docenti che insegnano nel Dipartimento di Matematica (54)
-SELECT * 
+SELECT DISTINCT `teachers`.`name`, `teachers`.`surname`
 FROM `teachers`
 JOIN `course_teacher`
 ON `teachers`.`id`= `course_teacher`. `teacher_id`
