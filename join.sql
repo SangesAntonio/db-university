6803cdffb2c7a185c21201cdab6d495888f7d6cc
SangesAntonio <sanges65@gmail.com>
2022-03-10 15:28:38 +0100

    added n.4

--- join.sql
+++ join.sql
@@ -22,6 +22,14 @@ ON `course_teacher`. `teacher_id`= `teachers`.`id`
 WHERE `teachers`.`name` = 'fulvio' AND `teachers`.`surname` = 'amato'
 
 --4. Selezionare tutti gli studenti con i dati relativi al corso di laurea a cui sono iscritti e il relativo dipartimento, in ordine alfabetico per cognome e nome
+SELECT `students`.`surname`, `students`.`name`,`degrees`.`name`,`departments`.`name`
+FROM `students`
+JOIN `degrees`
+ON `degrees`.`id`= `students`.`degree_id`
+JOIN `departments`
+ON `departments`.`id`= `degrees`.`department_id`
+ORDER BY `students`.`surname`, `students`.`name`
+
 --5. Selezionare tutti i corsi di laurea con i relativi corsi e insegnanti
 --6. Selezionare tutti i docenti che insegnano nel Dipartimento di Matematica (54)
 --7. BONUS: Selezionare per ogni studente quanti tentativi d’esame ha sostenuto per superare ciascuno dei suoi esami
